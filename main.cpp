b5affb61385e61d1830659fe43cbb05aa6565879
Bob(SoninAV) <bob@example.com>
2020-04-04 22:29:33 +0300

    code: + умножение

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter A and B: ";
-    int a, b;
-    cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'
-    << "A - B = " << a - b << '\n';
+    cout << "Hello world!" << endl;
     return 0;
 }
-
